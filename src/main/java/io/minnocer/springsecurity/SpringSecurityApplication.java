ce540c04c1f4e433c9bd217bd18816046cc98c47
Mustapha Innocer <mustaphainnocer@gmail.com>
2022-01-17 11:47:13 +0000

    added a test controller

--- src/main/java/io/minnocer/springsecurity/SpringSecurityApplication.java
+++ src/main/java/io/minnocer/springsecurity/SpringSecurityApplication.java
@@ -2,6 +2,7 @@ package io.minnocer.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -9,5 +10,4 @@ public class SpringSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
-
 }
